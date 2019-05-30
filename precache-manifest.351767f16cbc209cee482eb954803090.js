b6979fe60f678e593ae2365352393e4bc5bc7f7c
Teemu <teemuvest@fastmail.fm>
2019-05-30 21:55:31 +0300

    Updates

---
+++ precache-manifest.351767f16cbc209cee482eb954803090.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35ad5fb6db62b551712ef59cfcb75580",
+    "url": "/greenhousecoder/koodi2.git/index.html"
+  },
+  {
+    "revision": "d8d8cfacf56b900f3108",
+    "url": "/greenhousecoder/koodi2.git/static/css/main.a6e9332f.chunk.css"
+  },
+  {
+    "revision": "2c2d35045ec22a105640",
+    "url": "/greenhousecoder/koodi2.git/static/js/2.dbe68bbe.chunk.js"
+  },
+  {
+    "revision": "d8d8cfacf56b900f3108",
+    "url": "/greenhousecoder/koodi2.git/static/js/main.53e4ad8f.chunk.js"
+  },
+  {
+    "revision": "3d7e4bb16fe604a2df3d",
+    "url": "/greenhousecoder/koodi2.git/static/js/runtime~main.8aa83539.js"
+  },
+  {
+    "revision": "db29b234209b01b0d0f30ce868f49025",
+    "url": "/greenhousecoder/koodi2.git/static/media/bg.db29b234.png"
+  }
+]);
